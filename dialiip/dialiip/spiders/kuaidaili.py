438b18a22070f80e4251489a1c333e43c79bf809
zuiyimeihuashang <3316065997@qq.com>
2023-11-26 10:00:34 +0800

    new

--- dialiip/dialiip/spiders/kuaidaili.py
+++ dialiip/dialiip/spiders/kuaidaili.py
@@ -4,7 +4,7 @@ import scrapy
 class KuaidailiSpider(scrapy.Spider):
     name = "kuaidaili"
     allowed_domains = ["kuaidaili.com"]
-    start_urls = ["https://kuaidaili.com"]
+    start_urls = ["https://www.kuaidaili.com/free/"]
 
     def parse(self, response):
         pass
