1609db12c830ef7a43b5c39906ccac752e266d88
zuiyimeihuashang <3316065997@qq.com>
2023-11-26 23:41:32 +0800

    new

--- 实用的小程序/gzmu校园网登录.js
+++ 实用的小程序/gzmu校园网登录.js
@@ -4,7 +4,7 @@
 // @version      1.1
 // @description  ECUT校园网自动登录脚本
 // @author       inventor
-// @match        http://10.101.6.51/a79.html
+// @match        http://10.101.6.51/a79.htm
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=79.141
 // @grant        unsafeWindow
 // @license MIT
@@ -12,8 +12,8 @@
 
 /* globals jQuery, $, waitForKeyElements */
 
-var username = "202242100136"; // 学号
-var password = "230098"; // 密码
+var username = "2022XXXXXX"; // 学号
+var password = "23XXXX"; // 密码
 var port = 2; // 0办公网，1学生内网，2中国移动，3中国电信
 
 (function() {
@@ -22,8 +22,7 @@ var port = 2; // 0办公网，1学生内网，2中国移动，3中国电信
 
     // 添加一个load事件监听器，用于在页面加载完成后执行以下操作
     window.addEventListener('load', function() {
-        // 遍历 portSelectors 数组，使用每个选择器选择一个 input 元素，并将其 checked 属性设置为 true
-        $(portSelectors[port]).click(); // 选择运行商
+        $(portSelectors[port]).click(); // 选择运行商,通过单击选中
         $("input[name='DDDDD']").val(username); // 设置 input[name='DDDDD'] 元素的值，即用户名
         $("input[name='upass']").val(password); // 设置 input[name='upass'] 元素的值，即密码
         $("input[name='0MKKey']").click(); // 点击 input[name='0MKKey'] 元素，模拟用户输入密码
