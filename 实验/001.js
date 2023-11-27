7940bbdb05e1e8a6ae23e804a42fedd9b94703d8
zuiyimeihuashang <3316065997@qq.com>
2023-11-27 19:22:45 +0800

    new

--- 实验/001.js
+++ 实验/001.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         贵州民族大学校园网快速自动登录脚本
 // @namespace    https://github.com/zuiyimeihuashang/taobao_scrapy/blob/master/%E5%AE%9E%E7%94%A8%E7%9A%84%E5%B0%8F%E7%A8%8B%E5%BA%8F/gzmu%E6%A0%A1%E5%9B%AD%E7%BD%91%E7%99%BB%E5%BD%95.js
-// @version      1.2
+// @version      1.3
 // @description  ECUT校园网自动登录脚本
 // @author       inventor
 // @match        *://*.101.6.51/a79.htm*
@@ -13,7 +13,7 @@
 /* globals jQuery, $, waitForKeyElements */
 
 var username = "2022XXXXX"; // 学号
-var 密码 = "23XXXX"; // 密码
+var password = "23XXXX"; // 密码
 var port = 2; // 0办公网，1学生内网，2中国移动，3中国电信
 
 (function() {
@@ -22,10 +22,14 @@ var port = 2; // 0办公网，1学生内网，2中国移动，3中国电信
 
     // 添加一个load事件监听器，用于在页面加载完成后执行以下操作
     window.addEventListener('load', function() {
-        // 遍历 portSelectors 数组，使用每个选择器选择一个 input 元素，并将其 checked 属性设置为 true
-        $(portSelectors[port])。click(); // 选择运行商
-        $("input[name='DDDDD']")。val(username); // 设置 input[name='DDDDD'] 元素的值，即用户名
-        $("input[name='upass']")。val(密码); // 设置 input[name='upass'] 元素的值，即密码
-        $("input[name='0MKKey']")。click(); // 点击 input[name='0MKKey'] 元素，模拟用户输入密码
+        $(portSelectors[port]).click(); // 选择运行商
+        $("input[name='DDDDD']").val(username); // 设置 input[name='DDDDD'] 元素的值，即用户名
+        $("input[name='upass']").val(password); // 设置 input[name='upass'] 元素的值，即密码
+        $("input[name='0MKKey']").click(); // 点击 input[name='0MKKey'] 元素，模拟用户登录
     }, false);
 })();
+/*
+附：
+1. 设置开机自动启动Chrom浏览器，win+R 打开运行框输入 shell:startup回车，打开一个文件夹，右击Chrom浏览器复制Chrom快捷方式，粘贴到打开的文件夹中。
+2. 设置Chrom浏览器自动打开登录页面，设置->启动时->选择打开特定的网页->添加校园网登录页面。
+ */
