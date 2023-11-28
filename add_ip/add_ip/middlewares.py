352432ed0aadc3ce94790c64ea3436913e9d9e20
zuiyimeihuashang <3316065997@qq.com>
2023-11-28 23:00:47 +0800

    new

--- add_ip/add_ip/middlewares.py
+++ add_ip/add_ip/middlewares.py
@@ -78,6 +78,7 @@ class AddIpDownloaderMiddleware:
         # - or return a Request object
         # - or raise IgnoreRequest: process_exception() methods of
         #   installed downloader middleware will be called
+
         return None
 
     def process_response(self, request, response, spider):
@@ -87,6 +88,10 @@ class AddIpDownloaderMiddleware:
         # - return a Response object
         # - return a Request object
         # - or raise IgnoreRequest
+        cookies_dict ={
+            'cookie':'__51vcke__20L1wEeeGTFXijbh=c1c68ba9-91ce-55ee-b6c0-b5d6d829265c; __51vuft__20L1wEeeGTFXijbh=1696753781937; __root_domain_v=.zdaye.com; _qddaz=QD.145896753782028; lastSE=baidu; acw_tc=76b20f6617011833555744970e47752909b9361df36ce2b8e61708b3aa26a5; __51uvsct__20L1wEeeGTFXijbh=6; Hm_lvt_dd5f60791e15b399bf200ae217689c2f=1701165074,1701172792,1701178837,1701183357; _qdda=3-1.1; _qddab=3-uedsw4.lpignb01; ASPSESSIONIDSEASARTD=FOGGGDPCDCOEEIFBNFKFCPBM; __vtins__20L1wEeeGTFXijbh=%7B%22sid%22%3A%20%220e159238-3953-5ac8-bc3f-abcaf150bcb8%22%2C%20%22vd%22%3A%202%2C%20%22stt%22%3A%203784%2C%20%22dr%22%3A%203784%2C%20%22expires%22%3A%201701185160255%2C%20%22ct%22%3A%201701183360255%7D; Hm_lpvt_dd5f60791e15b399bf200ae217689c2f=1701183360',
+        }
+        request.cookies = cookies_dict
         return response
 
     def process_exception(self, request, exception, spider):
