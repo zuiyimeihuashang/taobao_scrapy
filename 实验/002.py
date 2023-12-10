e7acfdcfc386b84f2ca63d6c236adf5693cb58c7
zuiyimeihuashang <3316065997@qq.com>
2023-12-10 17:56:50 +0800

    new

--- 实验/002.py
+++ 实验/002.py
@@ -1,5 +1,26 @@
-import numpy as np
-n, k = map(int, input().split())
-arr = np.fromstring(input(), dtype = np.uint32, sep=' ')
-arr.sort()
-print(arr[k])
+a,b = map(int,input().split())
+list1=[]
+for i in range(a):
+    list1.append(list(map(int,input().split())))
+sum = 0
+try:
+    for i in range(1,b+1):
+        cnt =0
+        for j in range(a):
+            if list1[j][0] > list1[j][-1]:
+                cnt += list1[j].pop(-1)
+            else:
+                cnt += list1[j].pop(0)
+        # print("cnt:",cnt)
+        sum += cnt * (2**i)
+finally:
+    print(sum)
+
+# 5 5
+# 0 0 0 0 0
+# 0 0 0 0 1
+# 876 1 566 920 598
+# 259 945 123 659 997
+# 176 478 293 464 278
+
+#118214
