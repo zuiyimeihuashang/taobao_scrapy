a798f2464d217b614e59cfe1b3376142c6b93d5c
zuiyimeihuashang <3316065997@qq.com>
2024-04-21 17:00:18 +0800

    new

--- Java_project/shiyan/shiyan/src/shiyan.java
+++ Java_project/shiyan/shiyan/src/shiyan.java
@@ -1,6 +1,13 @@
 public class shiyan {
+    String a;
+    int b ;
     public static void main(String[] args){
         System.out.println("hello world!");
-        
+
+    }
+
+    public shiyan(String a, int b) {
+        this.a = a;
+        this.b = b;
     }
 }
