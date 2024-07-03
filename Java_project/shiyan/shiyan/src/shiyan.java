c1caa16a5acf80a4813c068ba8f01a5799d5eb27
zuiyimeihuashang <3316065997@qq.com>
2024-07-03 14:40:23 +0800

    new

--- Java_project/shiyan/shiyan/src/shiyan.java
+++ Java_project/shiyan/shiyan/src/shiyan.java
@@ -1,55 +1,179 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.Scanner;
-import java.io.*;
-import java.nio.file.*;
+class Point
+{
+    int[] x;
+    static double min=-5.12;
+    static double max=5.12;
+    static int size=22;
+    double fitness;
 
-
-public class shiyan {
-    double a;
-    double b;
-    double c;
-
-    public shiyan(double a, double b, double c) {
-        this.a = a;
-        this.b = b;
-        this.c = c;
+    public Point()
+    {
+        x=new int[size];
+        for(int i=0;i<size;i++)
+            if(Math.random()>0.5)
+                x[i]=1;
+            else
+                x[i]=0;
     }
 
-    public void print_shiyan() {
-        System.out.println(this.a);
-        System.out.println(this.b);
-        System.out.println(this.c);
+    public double decode()
+    {
+        double y=0;
+        for(int i=0;i<size;i++)
+        {
+            y=y+x[i]*Math.pow(2,size-1-i);
+        }
+        y=min+y*(max-min)/(Math.pow(2,size)-1.0);
+        return y;
     }
 
-    public double zhouchang() {
-        return this.a + this.b + this.c;
+    public double getFitness()
+    {
+        double vx=decode();
+        fitness=vx*vx;
+        return fitness;
     }
 
-    public double mianji() {
-        double p = this.zhouchang() / 2.0;
-        return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+}
+
+
+class Tools
+{
+
+    public static void copy(Point a,Point b)
+    {
+        for(int i=0;i<a.x.length;i++)
+        {
+            b.x[i]=a.x[i];
+        }
+        b.fitness=a.fitness;
     }
 
-    public boolean is_sanjiaoxing() {
-        double max1 = Math.max(this.a, this.b);
-        max1 = Math.max(max1, this.c);
-        double duan = this.zhouchang() - max1;
-        return duan > max1;
+
+    public static void swap(Point a,Point b)
+    {
+        Point temp=new Point();
+        copy(a,temp);
+        copy(b,a);
+        copy(temp,b);
     }
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        shiyan shiyan1 = new shiyan(scanner.nextDouble(), scanner.nextDouble(), scanner.nextDouble());
-        shiyan1.print_shiyan();
-        double b = shiyan1.zhouchang();
-        double d = shiyan1.mianji();
-        System.out.printf("zhouchang:%.2f\n", b);
-        System.out.printf("mianji:%.2f\n", d);
-        System.out.printf("is_sanjiaoxing:%b\n", shiyan1.is_sanjiaoxing());
-        Path path1 = Paths.get("D:\\Student\\file1");
-        path1 = Files.createDirectories(path1);
-        file = Files.createFile(path1);
+    public static int findBest(Point[] arr)
+    {
+        int pos=0;
+        double min=arr[pos].fitness;
+        for(int i=1;i<arr.length;i++)
+        {
+            if(min>arr[i].fitness)
+            {
+                pos=i;
+                min=arr[i].fitness;
+            }
+        }
+        return pos;
+
     }
 
+    public static void cross(Point a,Point b,Point ab,Point ba)
+    {
+        int pos=(int)(Math.random()*Point.size);
+        for(int i=0;i<pos;i++)
+        {
+            ab.x[i]=a.x[i];
+            ba.x[i]=b.x[i];
+        }
+        for(int i=pos;i<Point.size;i++)
+        {
+            ab.x[i]=b.x[i];
+            ba.x[i]=a.x[i];
+        }
+    }
+
+
+    public static void mutA(Point a)
+    {
+        Point temp=new Point();
+        copy(a,temp);
+        int pos=(int)(Math.random()*Point.size);
+        a.x[pos]=(a.x[pos]+1)%2;
+        if(a.getFitness()>temp.fitness)
+            a.x[pos]=(a.x[pos]+1)%2;
+    }
+}
+
+
+class GASet
+{
+    Point[][] obj;
+    int row,col;
+
+    public GASet(int r,int c)
+    {
+        row=r;
+        col=c;
+        obj=new Point[row][col];
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                obj[i][j]=new Point();
+    }
+
+
+    public void evolution()
+    {
+
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                obj[i][j].getFitness();
+
+
+        int best;
+        for(int i=0;i<row;i++)
+        {
+            best=Tools.findBest(obj[i]);
+            Tools.swap(obj[i][best],obj[i][i]);
+        }
+
+
+        for(int i=0;i<row;i++)
+            for(int j=i+1;j<col;j++)
+                Tools.cross(obj[i][i],obj[j][j],
+                        obj[i][j],obj[j][i]);
+
+
+        for(int i=0;i<row;i++)
+            for(int j=0;j<col;j++)
+                Tools.mutA(obj[i][j]);
+    }
+}
+
+public class shiyan
+{
+    public static void main(String[] arg)
+    {
+        GASet s=new GASet(20,20);
+        int n=0;
+        while(n<100)
+        {
+            s.evolution();
+            n++;
+        }
+        int allBest=findBest(s.obj);
+        System.out.println("Min="+s.obj[allBest][allBest].getFitness());
+    }
+
+    public static int findBest(Point[][] arr)
+    {
+        int pos=0;
+        double min=arr[pos][pos].fitness;
+        for(int i=1;i<arr.length;i++)
+        {
+            if(min>arr[i][i].fitness)
+            {
+                pos=i;
+                min=arr[i][i].fitness;
+            }
+        }
+        return pos;
+
+    }
 }
--- Java_project/weibo/src/main/java/com/ban/WeiboApp.java
+++ Java_project/weibo/src/main/java/com/ban/WeiboApp.java
@@ -1,2 +1,15 @@
-package com.ban;public class WeiboApp {
+package com.ban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.ArrayList;
+import java.util.List;
+@SpringBootApplication
+/*
+表示是SpringBootApplication的项目。
+*/
+public class WeiboApp {
+    public static void main(String[] args) {
+        SpringApplication.run(WeiboApp.class,args);
+    }
 }
